989f4d1af75782472552d9eebb604ce6c52022be
Joce <jocelyn.zaruma@proton.me>
2025-01-05 18:30:18 +0100

    Mise à jour du script pour gérer les chemins des fichiers avec des arguments.

--- programmes/make_pals_corpus.sh
+++ programmes/make_pals_corpus.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-# pour utiliser ce script pour avoir le format de PALS pour les fichiers DUMP
-# suit ce format ./make_pals_corpus.sh <dossier> <base>
-# taper dans la ligne de commande par exemple ./make_pals_corpus.sh "../dumps-text" "lang2"
-
-# pour utiliser ce script pour avoir le format de PALS pour les fichiers CONTEXTES
-# taper dans la ligne de commande par exemple ./make_pals_corpus.sh "../contextes" "lang2"
-# faire attention d'enlever la ligne de sortie pour la sortie de contextes dans le code et
-# commenter la ligne pour la sortie des fichiers dumps-text
-
 # CHECK ARGUMENTS
 if [[ $# -ne 2 ]]
 then
     echo "Il manque exactement deux arguments"
     echo "Utilisation : make_pals_corpus.sh <nom_de_dossier> <nom_de_base>" # ex: <dumps-text> <lang2>
-    # taper dans la ligne de commande par ex ./make_pals_corpus.sh "../dumps-text" "lang2" mais pour votre langue
     exit 1
 fi
 
@@ -23,13 +13,17 @@ fi
 dossier=$1
 base=$2
 
-# Définition des fichiers pertinents à la tâche
-fichiers=$(find "$dossier" -maxdepth 1 -type f -name "${base}-*.txt")
-contenu_counter=0
-
-# Chemins de sortie
-#sortie_pals_dumps="../pals/dumps-text-$base.txt"   # ex: dumps-text-lang2.txt
-sortie_pals_contextes="../pals/contextes-$base.txt" # ex: contextes-text-lang2.txt
+# Définition des fichiers et la sortie
+if [[ "$dossier" == "dumps-text" ]]
+then
+    fichiers=$(find "../$dossier" -maxdepth 1 -type f -name "${base}-*.txt")
+    sortie_pals_dumps="../pals/dumps-text-$base.txt"
+fi
+if [[ "$dossier" == "contextes" ]]
+then
+    fichiers=$(find "../$dossier" -maxdepth 1 -type f -name "${base}-*.txt")
+    sortie_pals_contextes="../pals/contextes-$base.txt"
+fi
 
 ## EXECUTION DE LA TÂCHE ##
 for fichier in $fichiers
